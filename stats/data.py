aec968680862c38cc467b866ee342701c5ba38b6
Forest Monk <skypather@gmail.com>
2019-11-03 17:22:11 -0600

    Completed module

--- stats/data.py
+++ stats/data.py
@@ -5,7 +5,7 @@ import pandas as pd
 game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE'))
 game_files.sort()
 
-game_frames = list()
+game_frames = []
 for game_file in game_files:
     game_frame = pd.read_csv(game_file, names = ['type', 'multi2', 'multi3', 'multi4', 'multi5', 'multi6', 'event'])
     game_frames.append(game_frame)
